f15f073b055beb1f0b4c3053e9746993f50ab4c6
gliechty <guy.liechty@gmail.com>
2016-11-30 13:39:27 -0700

    this should complete the save. All functions working

--- app.js
+++ app.js
@@ -29,13 +29,55 @@ var url = "mongodb://localhost:27017/restaurant_db";
 // });
 
 // user creates restaurant
+// mongo.connect(url, function(err, db){
+// 	var collection = db.collection('restaurants');
+// 	var name = prompt("Type the name of the restaurant, fool");
+// 	var address = prompt("Type the address too");
+// 	var yelp = prompt("Get the yelp url and paste it");
+// 	collection.insert({"name":name,"address":address,"yelp":yelp});
+// 	collection.find().toArray(function(err, doc){
+// 		console.log(doc);
+// 	});
+// });
+
+// user updates a restaurant
 mongo.connect(url, function(err, db){
 	var collection = db.collection('restaurants');
-	var name = prompt("Type the name of the restaurant, fool");
-	var address = prompt("Type the address too");
-	var yelp = prompt("Get the yelp url and paste it");
-	collection.insert({"name":name,"address":address,"yelp":yelp});
+	var which = prompt("Update name, address, or yelp?");
+	var toModify = prompt("Which restaurant would you like to update?");
+	var update = prompt("Enter the updated info here");
 	collection.find().toArray(function(err, doc){
-		console.log(doc);
-	});
-});
+		if (which == "name"){
+			collection.findOneAndUpdate({"name": toModify},{$set:{"name": toModify, "name":update}});
+		} else if (which == "address"){
+			collection.findOneAndUpdate({"name": toModify},{$set:{"name": toModify, "address":update}});
+		} else if (which == "yelp"){
+			collection.findOneAndUpdate({"name": toModify},{$set:{"name": toModify, "yelp":update}});
+		}
+	}
+)});
+
+// user deletes restaurant
+// mongo.connect(url, function(err, db){
+// 	var collection = db.collection('restaurants');
+// 	var name = prompt("Type the name of the restaurant you wish to delete");
+// 	db.collection('restaurants').remove({"name":name});
+// 	collection.find().toArray(function(err, doc){
+//       console.log(doc);
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
